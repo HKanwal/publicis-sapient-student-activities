fb2130d0795806ea4c5aa19a48dc821e94a08c22
HKanwal <hsk532991@gmail.com>
2022-06-27 16:13:48 -0400

    Added API tests using mock service.

--- day13/spring-testing-demo/src/test/java/com/example/springtestingdemo/SpringTestingDemoApplicationTests.java
+++ day13/spring-testing-demo/src/test/java/com/example/springtestingdemo/SpringTestingDemoApplicationTests.java
@@ -1,11 +1,13 @@
 package com.example.springtestingdemo;
 
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 @WebMvcTest(value = Restapi.class)
@@ -13,10 +15,14 @@ class SpringTestingDemoApplicationTests {
 
 	@Autowired
 	private MockMvc mvc;
-	
+
+	@MockBean
+	MyService myService;
+
 	@Test
 	public void testGreetings() throws Exception {
-		mvc.perform(get("/api/Sachin")).andExpect(content().string("Welcome Sachin"));
+		when(myService.welcome("Hark")).thenReturn("Welcome Hark");
+		mvc.perform(get("/api/Hark")).andExpect(content().string("WELCOME HARK"));
 	}
 
 }
