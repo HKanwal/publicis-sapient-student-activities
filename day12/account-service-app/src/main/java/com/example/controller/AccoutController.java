b195bd6576f0593f29f13e1c316776e045fa184a
HKanwal <hsk532991@gmail.com>
2022-06-27 15:00:55 -0400

    Day13: Change service-discovery port and experiment with deployment of MS on multiple ports.

--- day12/account-service-app/src/main/java/com/example/controller/AccoutController.java
+++ day12/account-service-app/src/main/java/com/example/controller/AccoutController.java
@@ -1,5 +1,6 @@
 package com.example.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,8 +9,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/account")
 public class AccoutController {
 
+	// Get port. @Value() gets application.properties props
+	// Multiple MS instances can be run on different machines/ports
+	// by specifying the port when executing the .jar.
+	@Value("${server.port}")
+	private String port;
+
 	@GetMapping
 	public String greet() {
-		return "Hello! this is Account Microservice";
+		System.out.println("Received get request.");
+		return "Hello! this is Account Microservice running on port: "+this.port;
 	}
 }
